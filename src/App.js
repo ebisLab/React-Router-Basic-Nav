7bdccdca1589e0720b61f38767cd70175acc0374
ebisLab <ebaiden@techie.com>
2020-01-27 18:08:35 -0500

    route declared

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
-    <Navigation />
+    <BrowserRouter >
+        <Navigation />
+
+        <Route path="/" component={About}></Route>
+    </BrowserRouter>
+
+
+
   </div>
 );
 
